8a822af48ca27a9c9606c694e1954e614d83ad5f
rangeesh <rangees28@gmail.com>
2018-08-24 18:20:26 +0530

    changes added

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+'''from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +6,9 @@ def index():
    return render_template('index.html')
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(debug = True)'''
+import requests as rq
+
+val=rq.get('https://api.github.com/users/mralexgray/repos')
+print(val.text)
+
