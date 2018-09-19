df07503ce1b19c93e2bd5f2a5d5143bc702e017b
rangeesh <rangees28@gmail.com>
2018-09-19 15:03:23 +0530

    improvements

--- Project_flask/main.py
+++ Project_flask/main.py
@@ -1,4 +1,6 @@
 from flask import *
+from jira.client import JIRA
+import urllib3
 app=Flask(__name__)
 @app.route('/')
 def index():
@@ -11,7 +13,16 @@ def validate():
 	if request.method =='POST':
 		user=request.form.get('username')
 		passw=request.form.get('pass')
-		return user+" "+passw
-	
+		try:
+			urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+			jira = JIRA(basic_auth=(user,passw),options={'server':'https://182.76.223.10:6060/','verify':False},max_retries=0)
+			issue = jira.issue('UN-9')
+			if jira.current_user():
+				response = redirect('http://54.174.153.99:9000')
+				response.headers = {'Authorization': '%s'%(jira.current_user())}  
+				return response
+		except:
+			return 'sorry username or password incorrect' 
+		
 if __name__=='__main__':
 	app.run()
